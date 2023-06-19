7354358954ba58b131be62be89852e86ba9bc122
maria buzina <buzinanm@yandex.ru>
2023-06-20 00:47:45 +0500

    modified files

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,9 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    example = Item("Смартфон", 10000, 20)
+    assert example.calculate_total_price() == 200000
+
+
