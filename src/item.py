3d14b1353743fb8c1ef16e7a83c569a838dcca19
maria buzina <buzinanm@yandex.ru>
2023-06-24 00:53:07 +0500

    homework_2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self.__name = new_name[:10]
+        else:
+            self.__name = new_name
+
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +47,19 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        with open('../src/items.csv', encoding='cp1251') as file:
+            extract = csv.DictReader(file)
+            for line in extract:
+                item = cls(line['name'], line['price'], line['quantity'])
+        return Item.all
+
+
+    @staticmethod
+    def string_to_number(value):
+        float_value = float(value)
+        return int(float_value)
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -6,4 +6,17 @@ def test_calculate_total_price():
     example = Item("Смартфон", 10000, 20)
     assert example.calculate_total_price() == 200000
 
+def test_set_name():
+    item = Item('Холодильник', 10000, 5)
+    item.name = 'Холодильник'
+    assert item.name == 'Холодильни'
+
+
+def test_string_to_number():
+    item = Item('Холодильник', 10000, 5)
+    assert item.string_to_number('5') == 5
+
+
+
+
 
