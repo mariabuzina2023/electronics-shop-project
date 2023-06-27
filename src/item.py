743a820b817f2ff44e3567b305184114a4c690fe
maria buzina <buzinanm@yandex.ru>
2023-06-27 21:05:12 +0500

    homework_3

--- src/item.py
+++ src/item.py
@@ -21,6 +21,14 @@ class Item:
         self.all.append(self)
 
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+
+    def __str__(self):
+        return f"{self.__name}"
+
+
     @property
     def name(self):
         return self.__name
@@ -55,7 +63,8 @@ class Item:
             extract = csv.DictReader(file)
             for line in extract:
                 item = cls(line['name'], line['price'], line['quantity'])
-        return Item.all
+        return item.all
+
 
 
     @staticmethod
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import csv
+
 
 
 def test_calculate_total_price():
@@ -15,6 +17,20 @@ def test_set_name():
 def test_string_to_number():
     item = Item('Холодильник', 10000, 5)
     assert item.string_to_number('5') == 5
+
+
+
+def test_repr():
+    example = Item("Смартфон", 10000, 20)
+    assert example.__repr__() == "Item('Смартфон', 10000, 20)"
+
+
+def test_str():
+    example = Item("Смартфон", 10000, 20)
+    assert example.__str__() == 'Смартфон'
+
+
+
 
 
 
