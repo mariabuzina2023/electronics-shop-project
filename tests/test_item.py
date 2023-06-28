894baf95eaeee714e8d41a717909b18ab1060198
maria buzina <buzinanm@yandex.ru>
2023-06-28 19:41:48 +0500

    homework_3

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,14 +10,25 @@ def test_calculate_total_price():
 
 def test_set_name():
     item = Item('Холодильник', 10000, 5)
+    item1 = Item('утюг', 2000, 4)
     item.name = 'Холодильник'
+    item1.name = 'утюг'
     assert item.name == 'Холодильни'
+    assert item1.name == 'утюг'
 
 
 def test_string_to_number():
     item = Item('Холодильник', 10000, 5)
     assert item.string_to_number('5') == 5
 
+
+# def test_instantiate_from_csv():
+
+def test_apply_discount():
+
+    item = Item('Холодильник', 10000, 5)
+    item.pay_rate = 1.0
+    assert item.price == 10000
 
 
 def test_repr():
